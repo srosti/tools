13f796846fde80aebc35e468f7d0b6b19b7d25e4
Shawn Rosti <srosti@cradlepoint.com>
2018-05-08 07:46:55 -0600

    Code to calculate MIC in a 4-way WPA key exchange.
    This is an example found on the web, however the
    example needed some tweaking. Changes made:
    
    1) Parameterize AP and STA nonce-values
    2) Insert the gtk key from AP into the 802.1x data
    frame to calculate the MIC for the 3rd exchange
    from STA to AP.

--- wifi/authenticate/generate_mic.py
+++ wifi/authenticate/generate_mic.py
@@ -60,42 +60,42 @@ def MakeMIC(pwd, ssid, A, B, data, wpa = False):
     return (mics, ptk, pmk)
 
 
-#Run a brief test showing the computation of the PTK, PMK, and MICS
-#for a 4-way handshake
+# Run a brief test showing the computation of the PTK, PMK, and MICS
+# for a 4-way handshake
 def RunTest():
     #the pre-shared key (PSK)
     psk = "12345678"
     #ssid name
     ssid = "blah"
-    #ANonce
-    aNonce = a2b_hex("f13374aea3749a0834591d0ca351b993181d5d75b02e41dd1eec9716ce479725")
-
-    #SNonce
-    sNonce = a2b_hex("862a4af51180f0715650906a3ac42842d68b2bde34099a0a08dde848801b4bdb")
-
-
-    #Authenticator MAC (AP)
+    # AP nonce-value. Sent from AP to STA in 1st handshake
+    aNonce = "e52a5a47414fa0aa063d6e9f373883859bb4aa4653787ae41590b381e8d7d719"
+    # Station nonce-value. Sent from STA to AP in 2nd handshake
+    sNonce = "c43f5b3e4c9a5da69461deab0d7cd5f989052633460fd1a4670e30d7794240e7"
+    # GTK is sent from AP to STA in 3rd handshake
+    gtk = "3f5e71350c509c719547a9877e76a6dfe6f69ba0a02a59226e7ea2574382b0be3978422fecd8e73957da91828ef1508ee094495e0e658fadadf687fd7f00bbd319424abb711e87082ff8258454565eae"
+    # Authenticator MAC (AP)
     apMac = a2b_hex("003044167b07")
-    #Station address: MAC of client
+    # Station address: MAC of client
     cliMac = a2b_hex("9801a79a63f3")
-    #The first MIC
-    mic1 = "fed4073e157107718a105b801fcc10ca"
-    #The entire 802.1x frame of the second handshake message with the MIC field set to all zeros
-    data1 = a2b_hex("0103007502010a00000000000000000001862a4af51180f0715650906a3ac42842d68b2bde34099a0a08dde848801b4bdb000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001630140100000fac040100000fac040100000fac020000")
 
 
-    #The second MIC
-    mic2 = "5b22728430c4a8ade8aae96b720e2872"
-    #The entire 802.1x frame of the third handshake message with the MIC field set to all zeros
-    data2 = a2b_hex("020300af0213ca00100000000000000002f13374aea3749a0834591d0ca351b993181d5d75b02e41dd1eec9716ce4797250000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050a26db0bc52202983e3b7fad7b196246fccec4e043644b8f83523765575b75a0d3cc5015cbbef6bf2502176ec9484d156f987ced5ad2022ee3c6130fa26b80744ac00b4852943777ffde2586fa6477aed")
+    # The first MIC. Sent from STA to AP
+    mic1 = "d2eb891a477f55086742cb788fdfe833"
+    # The entire 802.1x frame of the 2nd handshake message (STA to AP) with the MIC field set to all zeros
+    data1 = a2b_hex("0103007502010a00000000000000000001" + sNonce + "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001630140100000fac040100000fac040100000fac020000")
 
-    #The third MIC
-    mic3 = "c0a2374b4d578559fb7cf1d431086501"
-    #The entire 802.1x frame of the forth handshake message with the MIC field set to all zeros
-    data3 = a2b_hex("0103005f02030a00000000000000000002862a4af51180f0715650906a3ac42842d68b2bde34099a0a08dde848801b4bdb0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
+    # The 2nd MIC exchanged. Sent from AP to STA.  
+    mic2 = "3c08b974304ca344633b2ce32610ede7"
+    # The entire 802.1x frame of the third handshake message (AP to STA) with the MIC field set to all zeros
+    data2 = a2b_hex("020300af0213ca00100000000000000002" + aNonce + "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050" + gtk)
+
+    # The third MIC. Sent from STA to AP
+    mic3 = "ec7561585d60891b184977bb431ea47a"
+    #The entire 802.1x frame of the forth handshake message (STA to AP) with the MIC field set to all zeros
+    data3 = a2b_hex("0103005f02030a00000000000000000002" + sNonce + "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
 
     #Create parameters for the creation of the PTK, PMK, and MICs
-    A, B = MakeAB(aNonce, sNonce, apMac, cliMac)
+    A, B = MakeAB(a2b_hex(aNonce), a2b_hex(sNonce), apMac, cliMac)
     #Generate the MICs, the PTK, and the PMK
     mics, ptk, pmk = MakeMIC(psk, ssid, A, B, [data1, data2, data3])
     #Display the pairwise master key (PMK)
@@ -125,6 +125,8 @@ def RunTest():
     micStr = b2a_hex(mics[2]).decode().upper()[:-8]
     print("actual mic:\t" + micStr)
     print('MATCH\n' if micStr == mic3Str else 'MISMATCH\n')
+#    from pbkdf2 import PBKDF2
+#    temp = PBKDF2(psk, ssid, 4096).read(32)
     return
 
 RunTest()
