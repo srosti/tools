0e6d73040a5b25087ecfb876362cd29b93cfe2f6
Shawn Rosti <srosti@cradlepoint.com>
2019-04-02 13:47:54 -0600

    ipstack geolocation example

---
+++ ip-geolocation/ipstack.py
@@ -0,0 +1,14 @@
+import urllib.request
+import json
+
+ip_addr = '216.64.128.45'
+api_key = '78dc1382931b359b037e0eb2e4337167'
+result = urllib.request.urlopen("http://api.ipstack.com/{}?access_key={}".format(ip_addr, api_key)).read()
+result = result.decode('utf8')
+
+data = json.loads(result)
+
+if 'success' not in data:
+    print('Country Code={}'.format(data['country_code']))
+else:
+    print('Error retrieving Country Code for IP address: {}'.format(ip_addr))
