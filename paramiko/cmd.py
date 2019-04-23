c953debb76d54e511aa35606a2134826f65fdc8d
Shawn Rosti <srosti@cradlepoint.com>
2019-04-23 06:56:49 -0600

    example remote ssh command

--- paramiko/cmd.py
+++ paramiko/cmd.py
@@ -8,7 +8,7 @@ password = '1q2w3e4r'
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname, port, username, password)
-command = 'cat /config/wlan/radio/0/bss/0/wpapsk'
+command = 'cat /config/wlan/radio/0/bss/0/ssid'
 (stdin, stdout, stderr) = client.exec_command(command)
 for line in stdout.readlines():
     print(line)
